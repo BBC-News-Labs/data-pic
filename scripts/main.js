1ff8d5fa56c8c00f35b08d5686de7a749898a79e
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-07 14:40:51 +0000

    css adjustments and explainer fix

--- scripts/main.js
+++ scripts/main.js
@@ -649,8 +649,8 @@ alert("This tool will help you build a data pic. You have to use HTML and CSS co
             annotationArray.splice(i, 1);
             explainerArray.splice(i, 1);
             backgroundArray.splice(i, 1);
-            hides the first speech bubble when something else was changed but we want it to show another asset instead
-            eliminate hide error
+            // hides the first speech bubble when something else was changed but we want it to show another asset instead
+            // eliminate hide error
             if ($("#iframe-preview").contents().find(explainerArray[i - 1])) {
                 annotationArray[i-1].hide();
                 explainerArray[i-1].hide();
