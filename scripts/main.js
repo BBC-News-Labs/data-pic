46db45b81fde4a473f94dbcdc2e865a45b66b752
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-12 19:03:01 +0000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -628,9 +628,9 @@ alert("This tool will help you build a data pic. You have to use HTML and CSS co
     for (var i =0; i < originalArray.length; i++) {
 
          console.log('iframe prev ' + $("#iframe-preview").contents().find(changedArray[i]).text());
-         console.log('original array ' + originalArray);
-         console.log('original array item ' + originalArray[i]);
-         console.log('original array item text ' + originalArray[i].text());
+         // console.log('original array ' + originalArray);
+         // console.log('original array item ' + originalArray[i]);
+         // console.log('original array item text ' + originalArray[i].text());
 
        if (originalArray[i].text() !== $("#iframe-preview").contents().find(changedArray[i]).text()) {
         console.log('here now' + [i]);//BUGGER HERE IT FAILS
