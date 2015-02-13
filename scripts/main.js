ba45a83517a81234f79ef904cd92ec6f95af10f2
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-13 14:27:44 +0000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -261,12 +261,11 @@ var wtop; // preview window current top position
 // >>> Main functions
 
 var loadData = function(frame, html, css) {
-    css = ".h2_explainer--hide #h2_eplainer { display: none;}";
+    //css = ".h2_explainer--hide #h2_eplainer { display: none;}";
     // function that creates html document to be uploaded on iframe
     console.log('Any CSS', css);
-    $("#" + frame).prop("contentDocument").write('<style>');
-    $("#" + frame).prop("contentDocument").write($("#" + css).val());
-    $("#" + frame).prop("contentDocument").write('</style></head>');
+    $("#" + frame).prop("contentDocument").write('<style>' + css + '</style>');
+    $("#" + frame).prop("contentDocument").write('</head>');
     $("#" + frame).prop("contentDocument").write($("#" + html).val());
     $("#" + frame).prop("contentDocument").write('</html>');
     $("#" + frame).prop("contentDocument").close();
