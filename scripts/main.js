1de694798a6881155933d50d6ae8cccc29fef035
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-20 15:46:19 +0000

    Refactoring overlay code

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,8 @@ var loadData = function(frame, html) {
               '<img src="./images/background_overlay6.png" id="background_overlay_source" alt="source background overlay" style="width:43%;height:28px">"' );
 
         iframePreviewNode.append(overlaysNode);
+        alert(overlaysNode.html());
+        alert(iframePreviewNode.html());
         alert('appended');
     // if (typeof contentDocument !== 'undefined') {
     //     contentDocument.write('<style>');
