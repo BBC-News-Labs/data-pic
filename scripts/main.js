0236fcf6350acfc63802ffd8d039eb4524988751
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-23 10:21:45 +0000

    using get element

--- scripts/main.js
+++ scripts/main.js
@@ -300,7 +300,8 @@ $(document).on('ready', function(){
                         'display': 'block',
                         'position': 'absolute',
                         'top': x.top + y.top + 4 + 'px',
-                        'right': 90 + 'px'
+                        'right': 90 + 'px',
+                        'left': 0 + 'px'
                     });
 
 
