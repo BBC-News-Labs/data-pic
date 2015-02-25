da54a92f80857798cac444efea66497c656834cc
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-25 10:06:10 +0000

    fixing background

--- scripts/main.js
+++ scripts/main.js
@@ -309,7 +309,11 @@ if (w < 1200) {
                                 'left': '16px'
                             });
                         } else {
-
+                            console.log('y ' + y);
+                            console.log('x ' + x);
+                            console.log('height ' + myHeight);
+                            console,log('weight ' + myWidth);
+                            console.log('gets into this function');
                             $(backgroundArray[backgroundArray.length - 1]).css({
                                 'display': 'block',
                                 'position': 'absolute',
