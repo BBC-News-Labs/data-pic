c01c65751117fdbdec98edfcd5c2d77a411e1145
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-25 10:31:11 +0000

    fixing background

--- scripts/main.js
+++ scripts/main.js
@@ -325,7 +325,7 @@ if (w < 1200) {
                                 'display': 'block',
                                 'position': 'absolute',
                                 'top': y.top + 24 + 'px',
-                                'left': '5px',
+                                'left': '1px',
                                 'width': '560' + 'px',
                                 'height': '349' + 'px'
 
