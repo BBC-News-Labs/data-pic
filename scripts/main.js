03122ea42c7f86c10fef0fe432c5d56340870633
Melanie Moeller <melaniemoeller1@gmail.com>
2015-02-25 10:19:16 +0000

    fixing background

--- scripts/main.js
+++ scripts/main.js
@@ -308,10 +308,8 @@ if (w < 1200) {
                                 'top': x.top + y.top + 4 + 'px',
                                 'left': '16px'
                             });
-                        } else {
-                            console.log('y ' + y);
-                            console.log('x ' + x);
-                            console.log('gets into this function');
+                        } else if (!ie10andbelow) {
+
                             $(backgroundArray[backgroundArray.length - 1]).css({
                                 'display': 'block',
                                 'position': 'absolute',
@@ -320,6 +318,18 @@ if (w < 1200) {
                                 'width': myWidth + 2 + 'px',
                                 'height': myHeight
                             });
+                        } else {
+
+                            $(backgroundArray[backgroundArray.length - 1]).css({
+                                'display': 'block',
+                                'position': 'absolute',
+                                'top': y.top + 24 + 'px',
+                                'left': '10px',
+                                'width': 570 + 'px',
+                                'height': 349 + 'px'
+
+                            });
+
                         };
 
 
